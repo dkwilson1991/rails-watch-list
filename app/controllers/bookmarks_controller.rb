cd9fcb7ee50ba1fa1dfc86e0128b944c22922da4
Keita Wilson <dkwilson1991@gmail.com>
2022-11-11 16:20:54 +0900

    list added

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,32 +1,37 @@
 class BookmarksController < ApplicationController
+  before_action :set_bookmark, only: :destroy
+  before_action :set_list, only: [:new, :create]
+
   def new
-    @list = List.find(params[:list_id])
     @bookmark = Bookmark.new
   end
 
   def create
     @bookmark = Bookmark.new(bookmark_params)
-    @list = List.find(params[:list_id])
+    @bookmark.list = @list
     if @bookmark.save
-      redirect_to list_path(@list), status: :see_other
+      redirect_to list_path(@list)
     else
-      render :show, status: :unprocessable_entity
+      render :new
     end
   end
 
   def destroy
-    @bookmark = Bookmark.find(params[:id])
-    @list = @bookmark.list
-    if @bookmark.destroy
-      redirect_to list_path(@list), status: :see_other
-    else
-      render :show, status: :unprocessable_entity
-    end
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list), status: :see_other
   end
 
   private
 
   def bookmark_params
-    params.require(:movie).permit(:title, :overview, :poster_url, :rating)
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+
+  def set_bookmark
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  def set_list
+    @list = List.find(params[:list_id])
   end
 end
